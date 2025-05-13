78604851e9f6ed7d76579b7c6b31dd36e0c29b0e
vsmottin <vsmottin@outlook.com>
2025-05-12 21:36:00 -0300

    exercicios lista stl

--- prova_2/listaSTL/lista_stl/stl_list.cpp
+++ prova_2/listaSTL/lista_stl/stl_list.cpp
@@ -1,4 +1,7 @@
 #include "stl_list.hpp"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 vector<int> List::vet_to_vector(int* v, int n){
     vector<int> vectorNovo;
@@ -93,17 +96,82 @@ vector<string> List::vectorize_expression(string expression){
         
         if(expression[i] == ' '){
             v.push_back(aux);
+            aux.clear();
         } else{
             aux += expression[i];
         }
     }
 
-    v.push_back(aux);
+    if (!aux.empty()){
+        v.push_back(aux);
+    }
+
     return v;
 }
 
-float List::calc_posfix(string expression){
+#include <stack>
+#include <vector>
+#include <string>
+#include <cctype>
+#include <stdexcept>
 
+using namespace std;
+
+vector<string> split_expression(const string& expression) {
+    vector<string> tokens;
+    string current;
+    
+    for (char c : expression) {
+        if (c == ' ') {
+            if (!current.empty()) {
+                tokens.push_back(current);
+                current.clear();
+            }
+        } else {
+            current += c;
+        }
+    }
+    
+    if (!current.empty()) {
+        tokens.push_back(current);
+    }
+    
+    return tokens;
+}
+
+float calc_posfix(string expression) {
+    stack<float> nums;
+    vector<string> tokens = split_expression(expression);
+    
+    for (const string& token : tokens) {
+        if (isdigit(token[0])) {
+            nums.push(stof(token));
+        } else {
+            if (nums.size() < 2) {
+                throw runtime_error("Expressão inválida");
+            }
+            
+            float b = nums.top(); nums.pop();
+            float a = nums.top(); nums.pop();
+            
+            switch (token[0]) {
+                case '+': nums.push(a + b); break;
+                case '-': nums.push(a - b); break;
+                case '*': nums.push(a * b); break;
+                case '/': 
+                    if (b == 0) throw runtime_error("Divisão por zero");
+                    nums.push(a / b); 
+                    break;
+                default: throw runtime_error("Operador inválido");
+            }
+        }
+    }
+    
+    if (nums.size() != 1) {
+        throw runtime_error("Expressão inválida");
+    }
+    
+    return nums.top();
 }
 
 bool List::check_posfix(string expression){
